be4174937d8963aba3c482c784a015ee30e26bcd
garretblankenship <garret.blankenship@coderacademy.edu.au>
2019-07-05 12:24:09 +1000

    class 4

--- src/components/forms/BookmarkForm.js
+++ src/components/forms/BookmarkForm.js
@@ -3,47 +3,35 @@ import { createBookmark } from "./../../actions";
 import { connect } from "react-redux";
 import { Field, reduxForm } from "redux-form";
 
-const state = {
-    title: "",
-    url: "",
-    error: null,
-    fetching: false
-}
-
 class BookmarkForm extends Component {
-    state = {...state}
-
-    onInputChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value })
-    }
-
-    onFormSubmit = async (event) => {
-        event.preventDefault();
-        const { title, url } = this.state;
-        
-        try {
-            // const response = await LocalAPI.post(`/bookmarks`, { title, url });
-            this.props.createBookmark(title, url);
-            
-        } catch(error) {
-           //this.setState({ error });
-        }
+    onFormSubmit = async (formValues) => {
+        const { title, url } = formValues;
+        await this.props.createBookmark(title, url);
+        this.props.reset();
     }
 
     render() {
-        const { title, url, fetching } = this.state;
+        const { handleSubmit } = this.props;
 
         return(
-            <form onSubmit={this.onFormSubmit}>
+            <form onSubmit={handleSubmit(this.onFormSubmit)}>
                 <div>
                     <label>Title</label>
-                    <input type="text" name="title" value={title} onChange={this.onInputChange} />
+                    <Field
+                        name="title"
+                        component="input"
+                        type="text"
+                    />
                 </div>
                 <div>
                     <label>Url</label>
-                    <input type="text" name="url" value={url} onChange={this.onInputChange} />
+                    <Field
+                        name="url"
+                        component="input"
+                        type="text"
+                    />
                 </div>
-                {fetching ? <p>Creating...</p> : <input type="submit" value="Create" />}
+                <input type="submit" value="Create" />
             </form>
         );
     }
